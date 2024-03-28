ee09bdd6d3cfab0c4d2018ace93a51cd6d6e8a09
ashmaghimire8 <ashmaghimire8@gmail.com>
2024-03-28 14:25:35 +0545

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,93 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+
+// Define the Task class
+class Task {
+    private String name;
+    private int number;
+
+    public Task(int number,String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getNumber(){
+        return number;
+    }
+    @Override
+    public String toString() {
+        return number + "." + name; // Return the task name when toString() is called
+    }
+}
+class ToDo extends JFrame{
+    private DefaultListModel<Task> listModel;
+    private JList<Task> taskList;
+
+    public ToDo(){
+     setTitle("To-Do List");
+     setSize(300,300);
+     //centers the JFrame
+     setLocationRelativeTo(null);
+     setVisible(true);
+
+     listModel = new DefaultListModel<>();
+     taskList = new JList<>(listModel);
+
+     //scrollpane
+     JScrollPane scrollPane = new JScrollPane(taskList);
+     add(scrollPane, BorderLayout.CENTER);
+
+     //panel to add buttons
+     JPanel buttonPanel = new JPanel();
+     buttonPanel.setLayout(new GridLayout(1, 2));
+
+     JButton addButton = new JButton("Add Task");
+     addButton.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+             String taskName = JOptionPane.showInputDialog(null,"Enter the task name");
+             if(taskName != null && !taskName.isEmpty()){
+                 //new task object is created and added to list Model
+                 Task task = new Task(taskName);
+                 listModel.addElement(task);
+             }
+         }
+     });
+     buttonPanel.add(addButton);
+
+     JButton deleteButton = new JButton("Delete Task");
+     deleteButton.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+             int selectedIndex = taskList.getSelectedIndex();
+             if(selectedIndex != -1){
+                 listModel.remove(selectedIndex);
+             } else {
+                 JOptionPane.showMessageDialog(null, "Please select a task to remove");
+             }
+         }
+     });
+     buttonPanel.add(deleteButton);
+     add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+}
+public class Main {
+    public static void main(String args[]) {
+        //avoids concurrency issues
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                ToDo todoListGUI = new ToDo();
+                todoListGUI.setVisible(true);
+            }
+        });
+    }
+}
